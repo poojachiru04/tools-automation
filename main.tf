0c40aee54da20299cb35de4aa8fdf81739f81ff8
pooja <poojaskumara@gmail.com>
2024-06-06 16:35:31 +0530

    add expense terraform to pipelines

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "tools" {
   for_each = var.tools
   source   = "./module"
 
-  name = each.valu["name"]
+  name          = each.valu["name"]
   instance_type = each.value["instance_type"]
+  port_no       = each.valu["port_no"]
 }
--- module/main.tf
+++ module/main.tf
@@ -30,6 +30,14 @@ resource "aws_instance" "main" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
 
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type             = "persistent"
+    }
+  }
+
   tags = {
     Name      = var.name
   }
