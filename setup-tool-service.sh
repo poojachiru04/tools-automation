fe04b69b62e48caab45dd98d6057da8b23126910
pooja <poojaskumara@gmail.com>
2024-06-11 13:49:08 +0530

    vault create in gocd

--- setup-tool-service.sh
+++ setup-tool-service.sh
@@ -1,1 +1,2 @@
-ansible-playbook -i ${tool_name}-internal.poodevops.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e tool_name=${tool_name} setup-tool-service.yml -e vault_token=${vault_token}
+# ansible-playbook -i ${tool_name}-internal.poodevops.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e tool_name=${tool_name} setup-tool-service.yml -e vault_token=${vault_token}
+env | tee /tmp/sec
