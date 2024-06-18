6979635a3986fb5c0efbcfb8a9272837c13f6c6e
pooja <poojaskumara@gmail.com>
2024-06-18 09:52:56 +0530

    add grok exporter to prometheus

--- variables.tf
+++ variables.tf
@@ -1,24 +1,34 @@
 variable "tools" {
   default = {
+
     vault = {
       name          = "vault"
       instance_type = "t3.small"
-      port_no       = "8200"
+      port_no = {
+        vault = 8200
+      }
       policy_actions = []
     }
+
     prometheus = {
       name          = "prometheus"
       instance_type = "t3.small"
-      port_no       = ["9090", "9093", "9094"]
+      port_no = {
+        prometheus   = 9090
+        alertmanager = 9093
+      }
       policy_actions = [
-         "ec2:DescribeInstances",
-         "ec2:DescribeAvailabilityZones"
-        ]
+        "ec2:DescribeInstances",
+        "ec2:DescribeAvailabilityZones"
+      ]
     }
+
     grafana = {
-      name           = "grafana"
-      instance_type  = "t3.small"
-      port_no        = "3000"
+      name          = "grafana"
+      instance_type = "t3.small"
+      port_no = {
+        grafana = 3000
+      }
       policy_actions = []
     }
   }
